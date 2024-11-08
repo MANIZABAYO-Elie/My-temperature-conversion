a9339b401b54b65e8a97f517149c520e44f812e1
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-11-08 20:40:35 +0200

    file committed

--- script.js
+++ script.js
@@ -9,10 +9,17 @@ let temp;
 function convert(){
 
  if(toFahrenheit.checked){
+      temp = Number(textBox.value);
+      temp= temp*9/5 + 32
+      result.textContent = temp + "F"
+
  }
   else if(toCelcius.checked){
+    temp = Number(textBox.value);
+      temp= (temp-32)*5/9
+      result.textContent = temp + "C"
 
-  }else{
+    }else{
     result.textContent = "Select a unit"
   }
  }
