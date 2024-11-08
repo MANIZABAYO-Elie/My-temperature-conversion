b7543f4945cd3dba2ec167566b50b228173d294f
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-11-08 23:18:23 +0200

    file committed

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 
+// temperature conversion 
+
 const textBox = document.getElementById('textBox');
 const toFahrenheit = document.getElementById('toFahrenheit');
 const toCelcius = document.getElementById('toCelcius');
