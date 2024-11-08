0163c5fd62b1667869bc79927f0da20c010f0263
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-11-08 20:12:01 +0200

    file committed

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 
+
+const textBox = document.getElementById('textBox');
+const toFahrenheit = document.getElementById('toFahrenheit');
+const toCelcius = document.getElementById('toCelcius');
+const result = document.getElementById('result');
+
+
 function convert(){
-    
+
 }
