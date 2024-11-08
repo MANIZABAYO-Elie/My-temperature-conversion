ebc4d9d47ff4c3b926bf5d567606f5d4498f2973
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-11-08 20:24:29 +0200

    file committed

--- script.js
+++ script.js
@@ -4,8 +4,15 @@ const textBox = document.getElementById('textBox');
 const toFahrenheit = document.getElementById('toFahrenheit');
 const toCelcius = document.getElementById('toCelcius');
 const result = document.getElementById('result');
-
+let temp;
 
 function convert(){
 
-}
+ if(toFahrenheit.checked){
+ }
+  else if(toCelcius.checked){
+
+  }else{
+    result.textContent = "Select a unit"
+  }
+ }
