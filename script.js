6061ca5a592585135093ebefe2a0faa412c77c1f
MANIZABAYO-Elie <152684938+MANIZABAYO-Elie@users.noreply.github.com>
2024-11-09 23:32:51 +0200

    file committed

--- script.js
+++ script.js
@@ -1,27 +1,21 @@
+// temperature conversion
 
-
-// temperature conversion 
-
-const textBox = document.getElementById('textBox');
-const toFahrenheit = document.getElementById('toFahrenheit');
-const toCelcius = document.getElementById('toCelcius');
-const result = document.getElementById('result');
+const textBox = document.getElementById("textBox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelcius = document.getElementById("toCelcius");
+const result = document.getElementById("result");
 let temp;
-
-function convert(){
-
- if(toFahrenheit.checked){
-      temp = Number(textBox.value);
-      temp= temp*9/5 + 32
-      result.textContent = temp + "F"
-
- }
-  else if(toCelcius.checked){
+// function
+function convert() {
+  if (toFahrenheit.checked) {
     temp = Number(textBox.value);
-      temp= (temp-32)*5/9
-      result.textContent = temp + "C"
-
-    }else{
-    result.textContent = "Select a unit"
+    temp = (temp * 9) / 5 + 32;
+    result.textContent = temp + "F";
+  } else if (toCelcius.checked) {
+    temp = Number(textBox.value);
+    temp = ((temp - 32) * 5) / 9;
+    result.textContent = temp + "C";
+  } else {
+    result.textContent = "Select a unit";
   }
- }
+}
